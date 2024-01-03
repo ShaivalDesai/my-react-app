dbd4f6993b271241286333808297f2f50226cc4a
Shaival Desai <dshaival59@gmail.com>
2024-01-03 15:33:39 +0530

    reactJs

---
+++ src/Galery.js
@@ -0,0 +1,14 @@
+// function Img() {
+//   return(
+//   <img src="https://i.imgur.com/MK3eW3As.jpg" alt="jjjj" />
+//   );
+// }
+
+// export default function Gallery() {
+//   return(
+//   <div>
+//     <Img />
+//     <Img />
+//   </div>);
+// }
+
---
+++ src/Image.js
@@ -0,0 +1,9 @@
+export default function Image()
+{
+    return
+    (
+        <img
+        src="https://i.imgur.com/ZF6s192.jpg"
+        alt="grg" />
+    )
+}
